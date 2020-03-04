0758e58aec66865e0876cac90882ce46fa29009e
Harvey Rogers <harvey.rogers@urbanairship.com>
2020-03-03 16:28:44 -0800

    wip

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,24 @@ import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   const context = github.context
-  const currentSha = await kit.getSha()
+  // const currentSha = await kit.getSha()
+  const currentBranch = github.context.payload.ref as string
+  const {owner, repo} = context.repo
 
+  core.info(`Fetching commits for ${owner}/${repo}:${currentBranch}`)
 
-  console.log('github.context', JSON.stringify(github.context, null, 2))
+  const githubToken = kit.getInputSafe('github-token', {required: true})
+  const client = new github.GitHub(githubToken)
+  const commits = await client.repos.listCommits({
+    owner,
+    repo,
+    sha: currentBranch,
+    // TODO: use since
+    per_page: 3
+  })
+  for (const commit of commits) {
+    console.log('commit', commit.sha)
+  }
   // const latestCommit: string = 'asdf'
   // console.log('Hello cyber')
 }
