425bf2c7e6762c4be6966390f3b8e5e2bb42393d
Harvey Rogers <harvey.rogers@urbanairship.com>
2020-03-03 16:20:31 -0800

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import * as core from '@actions/core'
+import * as kit from '@harveyr/github-actions-kit'
+import * as github from '@actions/github'
+
 
 async function run(): Promise<void> {
-  console.log('Hello cyber')
+  const context = github.context
+  const currentSha = await kit.getSha()
+
+
+  console.log('github.context', JSON.stringify(github.context, null, 2))
+  // const latestCommit: string = 'asdf'
+  // console.log('Hello cyber')
 }
 
 run().catch(err => {
