62733ca516daac331908f60802d6e7c59747c24f
Harvey Rogers <harvey.rogers@urbanairship.com>
2020-03-03 17:20:24 -0800

    wip

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,29 @@ import * as core from '@actions/core'
 import * as kit from '@harveyr/github-actions-kit'
 import * as github from '@actions/github'
 
+function getCurrentBranch(): string {
+  const context = github.context
+  const { payload } = context
+
+  core.info(`FIXME: ${context.eventName}, ${context.action}`)
+
+  if (payload.pull_request) {
+    return payload.pull_request.head.ref as string
+  }
+  if (payload.ref) {
+    return payload.ref
+  }
+  throw new Error(
+    `I don't know how to find the current branch in payload: ${JSON.stringify(
+      context.payload,
+    )}`,
+  )
+}
+
 async function run(): Promise<void> {
   const context = github.context
   const { owner, repo } = context.repo
-  const currentBranch = context.payload.ref as string
-  if (!currentBranch) {
-    throw new Error(
-      `Branch not found in payload: ${JSON.stringify(context.payload)}`,
-    )
-  }
+  const currentBranch = getCurrentBranch()
 
   const currentSha: string = kit.getSha()
 
