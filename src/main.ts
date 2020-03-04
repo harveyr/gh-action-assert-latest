83b687ae750926fe9c7f8dbf35966b23e462102d
Harvey Rogers <harvey.rogers@urbanairship.com>
2020-03-03 16:35:24 -0800

    wip

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   const context = github.context
-  // const currentSha = await kit.getSha()
+  const currentSha = await kit.getSha()
   const currentBranch = github.context.payload.ref as string
   const {owner, repo} = context.repo
 
@@ -13,18 +13,27 @@ async function run(): Promise<void> {
 
   const githubToken = kit.getInputSafe('github-token', {required: true})
   const client = new github.GitHub(githubToken)
-  const commits = await client.repos.listCommits({
+  const resp = await client.repos.listCommits({
     owner,
     repo,
     sha: currentBranch,
     // TODO: use since
     per_page: 3
   })
+  const commits = resp.data
   for (const commit of commits) {
     console.log('commit', commit.sha)
   }
   // const latestCommit: string = 'asdf'
   // console.log('Hello cyber')
+
+  const latestSha = commits[0].sha
+
+  if (currentSha !== latestSha) {
+    core.setFailed(`Current SHA ${currentSha.slice(0, 7)} is behind ${latestSha.slice(0.7)}`)
+  } else {
+    core.info(`Current SHA ${currentSha.slice(0, 7)} is the latest commit.`)
+  }
 }
 
 run().catch(err => {
